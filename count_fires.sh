6ca862ff6fdd0758cebe6d6bc16f328c003c3d93
nmath020 <114368538+nfmathieu94@users.noreply.github.com>
2022-10-12 15:10:57 -0700

    First Draft

--- count_fires.sh
+++ count_fires.sh
@@ -4,28 +4,51 @@
 # download the CSV file
 # curl -o calfire.csv ...
 
+curl -o calfires.csv https://gis.data.cnra.ca.gov/datasets/CALFIRE-Forestry::recent-large-fire-perimeters-5000-acres.com
+
 # print out the range of years found -- you may need to go in and edit the file
 # cut -d, ....
 MINYEAR=1900
 MAXYEAR=1901
+
+echo -n | cut -d , -f2 calfires.csv | sort -n
+nano calfires.csv #deleted row data that was interfering with alignment
+
 # write code to set these variables with the smallest and largest years
+
+MINYEAR=$(cut -d , -f2 calfire.csv | sort -n | head -n 2)
+MAXYEAR=$(cut -d , -f2 calfire.csv | sort -n | tail -n 1)
 echo "This report has the years: $MINYEAR-$MAXYEAR"
 # if you have problems the CSV file already part of this repository so you can use 'calfires_2021.csv'
 
 # print out the total number of fires (remember to remove the header line)
 TOTALFILECOUNT=0
+
+echo -n | cut -d , -f2 calfires.csv | sort -n | tail -n +2 | wc -l
+
 # put your code here to update this variable
+
+TOTALFILECOUNT=$(cut -d , -f2 calfires.csv | sort -n | tail -n +2 | wc -l)
+
 echo "Total number of files: $TOTALFILECOUNT"
 
 # print out the number of fire in each year
-echo "Number of fires in each year follows:"
+
+$FIRES_EACH_YEAR=(cut -d , -f2 calfires.csv | sort -n | tail -n +2 | uniq -c)
+
+echo "Number of fires in each year follows: $FIRES_EACH_YEAR"
 
 
 # print out the name of the largest file use the GIS_ACRES and report the number of acres
+LARGEST=$(awk -F, '{print $13","$6} calfires.csv | sort -n | tail -n 1 | cut -d , -f2)
+LARGESTACRES=$(cut -d , -f13 calfires.csv | sort -n | tail -n 1)
 echo "Largest fire was $LARGEST and burned $LARGESTACRES"
 
 # print out the years - change the code in $(echo 1990) to print out the years (hint - how did you get MINYEAR and MAXYEAR?
-for YEAR in $(echo 1990)
+
+cut -d , -f2,13 calfires.csv | sort -n >> year_and_acres.csv
+
+for YEAR in $(year_and_acres.csv)
 do
 #      TOTAL=$(grep ... | awk ...)
       echo "In Year $YEAR, Total was $TOTAL"
